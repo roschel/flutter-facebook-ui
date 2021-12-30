61711b8bd47393772783b822aeccaa9a2d3da702
roschel <joaomroschel@gmail.com>
2021-12-30 19:50:46 -0300

    [STORIES] - include user name on the bottom

--- lib/widgets/stories.dart
+++ lib/widgets/stories.dart
@@ -18,7 +18,7 @@ class Stories extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 200.0,
-      color: Colors.purple,
+      color: Colors.white,
       child: ListView.builder(
         padding: const EdgeInsets.symmetric(
           vertical: 10.0,
@@ -102,6 +102,20 @@ class _StoryCard extends StatelessWidget {
                   imageUrl: story.user.imageUrl,
                   hasBorder: !story.isViewed,
                 ),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 8.0,
+          right: 8.0,
+          child: Text(
+            isAddStory ? "Add to Story" : story.user.name,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
         )
       ],
     );
