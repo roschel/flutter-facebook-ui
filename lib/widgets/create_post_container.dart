51621cfaa865aeff6b7e28c1873c82136759cba7
roschel <joaomroschel@gmail.com>
2021-12-30 10:17:56 -0300

    text field in post create

--- lib/widgets/create_post_container.dart
+++ lib/widgets/create_post_container.dart
@@ -13,6 +13,7 @@ class CreatePostContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 0.0),
       color: Colors.orange,
       child: Column(
         children: [
@@ -23,6 +24,13 @@ class CreatePostContainer extends StatelessWidget {
                 backgroundColor: Colors.grey[200],
                 backgroundImage:
                     CachedNetworkImageProvider(currentuser.imageUrl),
+              ),
+              const SizedBox(width: 8.0),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration.collapsed(
+                      hintText: "What's on your mind?"),
+                ),
               )
             ],
           )
